47896c9c1e4a7a77150fc7e651810d35dcd1587c
Golam Murshed <murshed394@gmail.com>
2024-06-13 10:27:00 +0530

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import investmentCalculatorlogo from '../assets/investment-calculator-logo.png'
+export default function Headers(){
+    return <header id="header">
+    <img src={investmentCalculatorlogo}/>
+    <h1>Investment calculator</h1>
+    </header>
+}
